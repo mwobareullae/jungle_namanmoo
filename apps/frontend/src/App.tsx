a7a68a972dafacafe7aa06c8670fab5cfef66b8e
jihyun <russee201916545@gmail.com>
2026-07-07 11:06:01 +0900

    feat(commerce): 장바구니와 주문서 UI 개선

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import AgentFloatingButton from "./components/AgentFloatingButton";
+import { HomeFooter } from "./components/HomeStaticSections";
 import { originalPages, type OriginalPageKey } from "./originalPages";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
@@ -547,6 +548,16 @@ function GlobalAgentEntry() {
   );
 }
 
+function GlobalFooter() {
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    return null;
+  }
+
+  return <HomeFooter />;
+}
+
 // 새 화면(/login, /signup, /signup/info)만 React Router로 연결하고, 나머지 기존 화면은 LegacyApp이 그대로 처리.
 function App() {
   return (
@@ -564,6 +575,7 @@ function App() {
         <Route path="/recommendation-guide" element={<RecommendationGuidePage />} />
         <Route path="*" element={<LegacyApp />} />
       </Routes>
+      <GlobalFooter />
       <GlobalAgentEntry />
     </>
   );
